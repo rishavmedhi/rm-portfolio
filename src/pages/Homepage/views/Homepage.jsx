cc2f4ad5072bf18cc99808b0f3861d8d63739ae0
Rishav Medhi <rishav164@gmail.com>
2020-11-20 23:21:03 +0530

    Setting up homepage container

---
+++ src/pages/Homepage/views/Homepage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Homepage extends Component {
+  render() {
+    return(
+      <div id="homepage-container">
+        HomePage
+      </div>
+    )
+  }
+}
