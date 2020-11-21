f1988e232bef1d403b65cc0f40ecdbab3e7c3197
Rishav Medhi <rishav164@gmail.com>
2020-11-21 23:48:07 +0530

    first page initial design

--- src/pages/Homepage/views/Homepage.jsx
+++ src/pages/Homepage/views/Homepage.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import '../styles/Homepage.style.scss';
 export default class Homepage extends Component {
   render() {
     return(
       <div id="homepage-container">
-        HomePage
+        <div class="top-banner-wrapper">
+          <div class="top-banner">
+            <h1>Hello, I'm Rishav Medhi</h1>
+            <div>
+              Full Stack Developer
+            </div>
+            <nav>
+              <ul class="nav-tabs">
+                <li>About Me</li>
+                <li>Github</li>
+                <li>LinkedIn</li>
+                <li>Writing</li>
+                <li>Contact Me</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     )
   }
