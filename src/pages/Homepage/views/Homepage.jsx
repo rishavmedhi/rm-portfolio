b22ce5b095335814e30215edae4b8cd3d0596e64
Rishav Medhi <rishav164@gmail.com>
2021-01-24 20:36:38 +0530

    about me content structure

--- src/pages/Homepage/views/Homepage.jsx
+++ src/pages/Homepage/views/Homepage.jsx
@@ -37,7 +37,18 @@ export default class Homepage extends Component {
         </div>
         <main>
           <div className="about-me-wrapper">
-            <h2>About Me</h2>
+            <h1>About Me</h1>
+            <div className="content">
+              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              </p>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
           </div>
         </main>
       </div>
