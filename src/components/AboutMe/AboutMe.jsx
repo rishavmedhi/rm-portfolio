d7aeae5af580de7cdc055006b1d8562c6c44e2e5
Rishav Medhi <rishav164@gmail.com>
2021-05-22 17:04:52 +0530

    adding target blank and rel param, styling for about me links

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,12 +7,12 @@ const AboutMe = (props) => {
       <h1>About Me</h1>
       <div className="content">
         <p> 
-          I&apos;m a full-stack-developer at <a href="https://zolostays.com/">Zolo</a>.
+          I&apos;m a full-stack-developer at <a target="_blank" rel="noreferrer nofollow" href="https://zolostays.com/">Zolo</a>.
           <br/>
           I am currently working on projects in Nodejs, Express and React.
         </p>
-        <p>I also photograph sunsets and cool stuff around me. Check out my <a href="https://www.instagram.com/the_clicky_sight/">Instagram</a>.</p>
-        <p>You can find me on <a href="https://twitter.com/RishavMedhi">Twitter</a>, <a href="https://github.com/rishavmedhi">Github</a> and <a href="https://www.linkedin.com/in/rishavmedhi/">Linkedin</a></p>
+        <p>I also photograph sunsets and cool stuff around me. Check out my <a href="https://www.instagram.com/the_clicky_sight/" target="_blank" rel="noreferrer">Instagram</a>.</p>
+        <p>You can find me on <a href="https://twitter.com/RishavMedhi" target="_blank" rel="noreferrer">Twitter</a>, <a href="https://github.com/rishavmedhi" target="_blank" rel="noreferrer">Github</a> and <a href="https://www.linkedin.com/in/rishavmedhi/" target="_blank" rel="noreferrer">Linkedin</a></p>
       </div>
     </div>    
   )
