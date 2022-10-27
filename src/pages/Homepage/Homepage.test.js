13f3103f3a97ca485ba4d7cd3d934f6971ce6d0a
Rishav Medhi <rishav164@gmail.com>
2022-10-28 01:38:54 +0530

    converting the homepage component to functional component, scroll UI test cases

--- src/pages/Homepage/Homepage.test.js
+++ src/pages/Homepage/Homepage.test.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Homepage from './views/Homepage';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Homepage from "./views/Homepage";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+const realScrollIntoViewPrototype = HTMLElement.prototype.scrollIntoView;
+var scrollIntoViewMock;
+
+// making a mock function for scrollIntoView
+beforeEach(() => {
+  scrollIntoViewMock = jest.fn();
+  HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
+});
+
+// replacing the scrollIntoView Mock fn back to the actual HTML DOM Prototype
+afterEach(() => {
+  HTMLElement.prototype.scrollIntoView = realScrollIntoViewPrototype;
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<Homepage />, div);
+});
+
+it("scrolling to writing section on clicking writing nav link", () => {
+  render(<Homepage />);
+  const writingNavElem = screen.getByTestId("nav-writing");
+  fireEvent.click(writingNavElem);
+  expect(scrollIntoViewMock).toBeCalledWith({ behavior: "smooth" });
+});
+
+it("scrolling to writing section on clicking down arrow icon", () => {
+  render(<Homepage />);
+  const downIconElem = screen.getByTestId("down-icon");
+  fireEvent.click(downIconElem);
+  expect(scrollIntoViewMock).toBeCalledWith({ behavior: "smooth" });
+});
+
+it("scrolling to writing section on clicking about-me nav link", () => {
+  render(<Homepage />);
+  const aboutMeNavElem = screen.getByTestId("nav-about-me");
+  fireEvent.click(aboutMeNavElem);
+  expect(scrollIntoViewMock).toBeCalledWith({ behavior: "smooth" });
 });
